3f8a74da3f615cc160f05670afdc61e44253acd0
holleyc <chris57575@hotmail.com>
2025-12-17 18:35:35 -0500

    Add UI Binding Benefits to Context

--- CSharpGames/obj/Debug/net8.0/CSharpGames.AssemblyInfo.cs
+++ CSharpGames/obj/Debug/net8.0/CSharpGames.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70790ded56f0dfd5080828ad9a3d7b1e1721ee53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bffe9896b18db1844a6fcd925295ceaa34b62d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
